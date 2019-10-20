ea61f02aa5b898a91212f4226d47f5f8e0d5264d
xiantang <zhujingdi1998@gmail.com>
2019-10-20 22:53:05 +0800

    Initial commit

---
+++ app/controllers/Application.scala
@@ -0,0 +1,17 @@
+package controllers
+import javax.inject.{Inject, Singleton}
+import play.api.mvc._
+@Singleton
+class Application @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+  /**
+   * Create an Action to render an HTML page with a welcome message.
+   * The configuration in the `routes` file means that this method
+   * will be called when the application receives a `GET` request with
+   * a path of `/`.
+   */
+  def index = Action {
+    Ok("It done!")
+  }
+
+}
