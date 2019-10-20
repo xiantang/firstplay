39750780c4b3edee5ea05cbfe47a57226b11f7f7
xiantang <zhujingdi1998@gmail.com>
2019-10-20 23:24:46 +0800

    add file

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -14,4 +14,30 @@ class Application @Inject()(cc: ControllerComponents) extends AbstractController
     Ok("It done!")
   }
 
+  // result的content type是从你指定的响应体的Scala值中自动推断的。
+  // Content-Type: application/xml;
+  def text = Action{
+    Ok(<message>Hello World!</message>)
+  }
+
+  def html = Action {
+    Ok(<h1>Hello World!</h1>).as(HTML)
+  }
+
+  def withHeaders = Action{
+    Ok("Hello World").withHeaders(
+      CACHE_CONTROL -> "max-age=3600",
+      ETAG -> "xx"
+    )
+  }
+
+  def cookie = Action {
+    val result = Ok("Hello World").withCookies(
+      Cookie("theme","blue")
+    )
+    val result1 = result.discardingCookies(DiscardingCookie("theme"))
+    result1
+  }
+
+
 }
